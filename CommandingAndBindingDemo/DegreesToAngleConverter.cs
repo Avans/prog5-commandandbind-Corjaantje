58d2a1389762c26bfb87f42c1d0681fe2e6d7f96
Corjaantje <choskam@gmail.com>
2016-09-27 12:17:24 +0200

    Finished \o/

--- CommandingAndBindingDemo/DegreesToAngleConverter.cs
+++ CommandingAndBindingDemo/DegreesToAngleConverter.cs
@@ -14,7 +14,7 @@ namespace CommandingAndBindingDemo
             //In deze methode converteer ik de temperatuur naar de juiste hoek voor het plaatje zodat de temp overeenkomt.
             //De waardes zijn door 'trial and error' aangepast zodat het lijtk dat de meter op de juiste temp staat
             double result = (double)value;
-            return result * 2.30 - 63.049;
+            return result * 2.30/2.50 - 63.049;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
---
+++ CommandingAndBindingDemo/DegreesToFahrenheid.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace CommandingAndBindingDemo
+{
+    class DegreesToFahrenheid : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return (double)value * 1.8000f + 32;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
